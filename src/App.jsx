2a3cb68679577fbdfe40f2b766c7caef665809ed
dteoman <deryateoman@gmail.com>
2025-04-14 00:12:35 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '4rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Welcome to RAFHAUS</h1>
+      <p>This is your React + Vite powered configurator base.</p>
+    </div>
+  )
+}
